db6a39175faf9e610bbc16f5c054483cee8eaae8
Rupeshnnn <77480014+Rupeshnnn@users.noreply.github.com>
2021-12-02 09:54:21 +0545

    Delete temp.js

--- temp.js
+++
@@ -1,9 +0,0 @@
-function celsius( fahrenheit){
-      return (fahrenheit-32)*5/9;
-}
-function kelvin( fahrenheit){
-    return (fahrenheit-273.15)* 9/5;
-}
-
-let fahrenheit = parseFloat(window.prompt("Enter value of fahrenheit:"))
-document.write("celsius value:"+celsius(fahrenheit)+" "+"Kelvin value:"+kelvin( fahrenheit))
